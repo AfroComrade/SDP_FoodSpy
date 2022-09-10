5587c8f7981a7c498b9ef0fc7b8ca9491d2a6ad1
Jcobman <vgm8024@autuni.ac.nz>
2022-09-10 14:58:11 +1200

    favicon
    favicon

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -4,7 +4,18 @@ import React from "react";
 const About = () => {
   return (
     <>
+    <div style={{
+            paddingTop: '250px',
+            textAlign: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            
+        }}>
       <h1 style = {{ color:"white", fontSize: 60, fontWeight: "bold"}}>This is an about page</h1>
+      </div>
+      <div style={{
+            paddingTop: '250px'         
+        }}></div>
     </>
   );
 };
