659be2a17aca2f851ad2c7daffc722b484545fc5
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-09-12 16:04:22 +1200

    update potential typo

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname+'/../frontend/build'));
     app.get('*', (req, res) => {
         res.sendFile(__dirname+'/../frontend/build/index.html');
-    })
+    });
 } else {
     app.get('/', (req, res) => res.send(`API running on post ${port}`));
 }
