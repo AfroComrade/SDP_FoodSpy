980aa59374c339ebd75a9bc699df581d38b5b373
CDGibson97 <105181677+CDGibson97@users.noreply.github.com>
2022-09-13 21:02:59 +1200

    Added disclaimers
    To protect us from getting in trouble.

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -11,7 +11,7 @@ const About = () => {
             alignItems: "center",
             
         }}>
-      <h1 style = {{ color:"white", fontSize: 60, fontWeight: "bold"}}>This is an about page</h1>
+      <h1 style = {{ color:"white", fontSize: 60, fontWeight: "bold"}}>This is for a university project, and prices displayed maybe using dummy data, or may be outdated. Please refer to the supermarkets website for current pricing.</h1>
       </div>
       <div style={{
             paddingTop: '250px'         
