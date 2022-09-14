ffce40a8bf6b3b5cbb1f959f4e08164b6cd3ae29
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-09-14 13:20:33 +1200

    we trying anything

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ app.use('/api/products', products);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname+'/../frontend/build'));
     console.log("sup callum 2");
-    app.get('/*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html')
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, '/../frontend/build', 'index.html')
     });
 } else {
     //app.get('/', (req, res) => res.send(`API running on port ${port}`));
