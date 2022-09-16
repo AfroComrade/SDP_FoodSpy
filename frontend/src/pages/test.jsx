6608b37cccb4a2682752500e35c33d697bd0d1df
CDGibson97 <105181677+CDGibson97@users.noreply.github.com>
2022-09-16 17:31:43 +1200

    Created Recipe Page
    Added Spoonacular API compatibility, and started creating the Webpage around this.

---
+++ frontend/src/pages/test.jsx
@@ -0,0 +1,13 @@
+import Popular from "../components/popularRecipes";
+
+import React from "react";
+
+function Test() {
+    return (
+        <div>
+            <Popular /> 
+        </div>
+    );
+}
+
+export default Test; 
