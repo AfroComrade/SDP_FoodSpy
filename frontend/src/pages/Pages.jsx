41b7042a4b5ff10c0c7783b9ac3178c485fa7934
Jcobman <vgm8024@autuni.ac.nz>
2022-09-19 19:38:35 +1200

    Updated naming standards/created account pages
    - updated naming standards and creates pages for create account and forgot password
    - Updated about page formatting and added logo image

--- frontend/src/pages/Pages.jsx
+++ frontend/src/pages/Pages.jsx
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import {  Routes, Route } from "react-router-dom";
 import "../App.css";
 
-import Home from "./home";
-import Footer from "./footer";
-import Menu from './menu';
-import About from './about';
+import Home from "./Home";
+import Footer from "./Footer";
+import Menu from './Menu';
+import About from './About';
 import SavedRecipes from './SavedRecipes';
 import RecipeDatabase from './RecipeDatabase';
 import AddRecipe from './AddRecipe';
 import Recipe from './Recipe';
 import Searched from './SearchPage';
+import ForgotPassword from './ForgotPassword';
+import CreateAcc from './CreateAcc';
 
 function Pages() {
   return (
@@ -18,12 +20,14 @@ function Pages() {
     <Menu />
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="about" element={<About />}/>
-        <Route path="recipes_saved" element={<SavedRecipes />}/>
-        <Route path="recipes_database" element={<RecipeDatabase/>}/>
-        <Route path="recipes_add" element={<AddRecipe/>}/>
-        <Route path="recipe/:name" element={<Recipe />}/>
-        <Route path="searched/:search" element={<Searched />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/recipes_saved" element={<SavedRecipes />}/>
+        <Route path="/recipes_database" element={<RecipeDatabase/>}/>
+        <Route path="/recipes_add" element={<AddRecipe/>}/>
+        <Route path="/recipe/:name" element={<Recipe />}/>
+        <Route path="/searched/:search" element={<Searched />}/>
+        <Route path="/forgot_password" element={<ForgotPassword />}/>
+        <Route path="/create_account" element={<CreateAcc />}/>
       </Routes>
       <Footer />
     </div>
