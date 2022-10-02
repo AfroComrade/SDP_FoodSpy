d6a3eec858f68e6d6c655e7cd86cd0431cf024b7
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-10-03 09:01:30 +1300

    Attempting to fix a few render crashes in the unit test

--- frontend/src/components/popularRecipes.jsx
+++ frontend/src/components/popularRecipes.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-import '@splidejs/react-splide/css';
 import {Link} from 'react-router-dom';
+
+// Commenting out because it breaks on unit tests
+// import '@splidejs/react-splide/css';
 
 // This component renders a random selection of recipes, with the intention of it becoming something 
 // that can be used to find specific types of recipes eg. Breakfast, Lunch and Dinner options 
