80a384663a3f7496a662d364f704866288868ad3
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-10-11 11:08:02 +1300

    Attempt to fail ci test

--- backend/utils/products.test.js
+++ backend/utils/products.test.js
@@ -3,18 +3,19 @@ const axios = require('axios');
 async function testCall() {
     try {
         axios.get(
-            'http://sdpfoodspy.herokuapp.com/api/products/product/Ace%20Gloves%20Small')
+            'http://sdpfodspy.herokuapp.com/api/products/product/Ace%20Gloves%20Small')
         .then(res => {
             data = res.data;
-            console.log(data);
             console.assert(data.imageURL === 'https://assets.woolworths.com.au/images/2010/671469.jpg?impolicy=wowcdxwbjbx&w=1200&h=1200');
             if (data.product !== 'Ace Gloves Smal')
             {
-                console.log("Api call successful!");
+                console.log("Api call successful");
             }
         })
     } catch (e) {
-        console.log(e);
+        console.log("Error found");
+        //console.log(e);
+        throw "Failed test!";
     }
 }
 
