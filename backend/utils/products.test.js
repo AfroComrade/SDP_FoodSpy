54bffda6690195ed59a71264615731c875ce09e1
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-10-11 11:10:28 +1300

    Another fail attempt

--- backend/utils/products.test.js
+++ backend/utils/products.test.js
@@ -1,22 +1,15 @@
 const axios = require('axios');
 
 async function testCall() {
-    try {
-        axios.get(
-            'http://sdpfodspy.herokuapp.com/api/products/product/Ace%20Gloves%20Small')
-        .then(res => {
-            data = res.data;
-            console.assert(data.imageURL === 'https://assets.woolworths.com.au/images/2010/671469.jpg?impolicy=wowcdxwbjbx&w=1200&h=1200');
-            if (data.product !== 'Ace Gloves Smal')
-            {
-                console.log("Api call successful");
-            }
-        })
-    } catch (e) {
-        console.log("Error found");
-        //console.log(e);
-        throw "Failed test!";
-    }
-}
+    axios.get(
+        'http://sdpfodspy.herokuapp.com/api/products/product/Ace%20Gloves%20Small')
+    .then(res => {
+        data = res.data;
+        console.assert(data.imageURL === 'https://assets.woolworths.com.au/images/2010/671469.jpg?impolicy=wowcdxwbjbx&w=1200&h=1200');
+        if (data.product !== 'Ace Gloves Smal')
+        {
+            console.log("Api call successful");
+        }
+})};
 
 testCall();
