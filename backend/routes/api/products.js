1027b8fd6b566ffcdb2693f264d4ff0450b5db47
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-10-25 20:09:47 +1300

    Direct commit for updating products code craft
    Based on the barely-passing code review lol

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -3,64 +3,73 @@ const router = express.Router();
 
 const firestore = require('../../firebase/firestore');
 
+// We're going to use the list of items saved from the scraper
 const itemstxt = 'scripts/firebase/items.txt';
 var fs = require('fs');
 
-async function getMatches(res, string) {
-    let objects = [];
+/*  GetMatches()
+    Input: String of words searched on the front end
+    Output: Array of JSONs that contain all words from the given string
+
+    This works by taking in 'item lines' from the items.txt. Each line is an item searchable on firebase
+    For each 'word' in the search string, check that it exists in the item line. 
+    If all words exist in the item line, add it to a list of 'items found' that will then be retrieved from firebase
+*/
+async function getMatches(res, inputString) {
+    let returnItemJsons = [];
     
+    //  Read in the 'item lines' from firebase
     fs.readFile(itemstxt, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return;
         }
 
-        list = data.split('\n');
-        words = string.split(' ');
+        // Split each line from items.txt into a separate entry
+        itemLines = data.split('\n');
 
-        founds = [];
+        // Split the input string into individual words
+        words = inputString.split(' ');
+
+        // Create an array of found items
+        foundItems = [];
         
-        for (let e = 0; e < list.length; e++) {
-            found = [words.length];
+        for (let e = 0; e < itemLines.length; e++) 
+        {
+
+            let found = true;
             for (let i = 0; i < words.length; i++)
             {
-                if (list[e].toLowerCase().includes(words[i].toLowerCase())) {
-                    found[i] = true;
-                }
-                else
-                {
-                    found[i] = false;
+                // If we ever find that a word is not in the item lines, just move on to the next item line
+                if (!itemLines[e].toLowerCase().includes(words[i].toLowerCase())) {
+                    found = false;
+                    break;
                 }
             }
 
-            allfound = true;
-            for (let i = 0; i < words.length; i++)
+            if (found)
             {
-                if (!found[i])
-                    allfound = false;
-            }
-            
-            if (allfound)
-            {
-                founds.push(list[e]);
-                //founds.push(list[e].slice(0, -1));
+                foundItems.push(itemLines[e]);
+                // The below code is used when testing locally
+                //foundItems.push(itemLines[e].slice(0, -1));
             }
         }
-        
 
         let promises = [];
-
-        for (let e = 0; e < founds.length; e++){
-            console.log(founds[e]);
-            promises.push(firestore('items', founds[e])
+        
+        // For each item found, make a request to firestore 'items' database to retreive said item then add it to the return Items
+        for (let e = 0; e < foundItems.length; e++){
+            console.log(foundItems[e]);
+            promises.push(firestore('items', foundItems[e])
                 .then(data => {
-                    objects.push(data)
+                    returnItemJsons.push(data)
                 })
             );
         }
 
-        Promise.all(promises).then((values) => {
-            res.send(objects);
+        // Wait until all requests have been made, then return the found items with the response
+        Promise.all(promises).then(() => {
+            res.send(returnItemJsons);
         });
     });
 }
